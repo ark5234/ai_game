1d6cd10ce06593a2cfcb70af69b0f2ade0145e0f
Vikrant Kawadkar <114129298+ark5234@users.noreply.github.com>
2025-04-08 11:03:24 +0530

    discard dump

--- tempCodeRunnerFile.py
+++
@@ -1,4 +0,0 @@
-def draw_mp_bar(x, y, mp):
-    pygame.draw.rect(screen, GRAY, (x, y, 200, 10))
-    pygame.draw.rect(screen, BLUE, (x, y, 2 * mp, 10))
-    display_text(f"{mp}/50", x + 70, y, font, WHITE)
